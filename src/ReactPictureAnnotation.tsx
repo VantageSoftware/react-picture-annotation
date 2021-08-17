af82e32b395e44460aa1e39e2c61fbf68f462aff
Anna Gadacz <anna.gadacz@cognite.com>
2021-08-17 16:53:30 +0200

    fix(beta): change pdfjs worker host to cdf hub

--- src/ReactPictureAnnotation.tsx
+++ src/ReactPictureAnnotation.tsx
@@ -13,7 +13,7 @@ import Transformer, { ITransformer } from "./Transformer";
 import styled from "styled-components";
 import { PDFPageProxy, PDFDocumentProxy } from "pdfjs-dist/types/display/api";
 
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/2.6.347/pdf.worker.min.js`;
+pdfjs.GlobalWorkerOptions.workerSrc = `https://cdf-hub-bundles.cogniteapp.com/dependencies/pdfjs-dist@2.6.347/build/pdf.worker.min.js`;
 import ArrowBox from "./ArrowBox";
 import { CogniteAnnotation } from "@cognite/annotations";
 
